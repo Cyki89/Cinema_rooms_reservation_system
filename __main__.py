3e47f53acef58f4fb30d070381cdb4f55546a93e
Jakub Cykowski <52428231+Cyki89@users.noreply.github.com>
2020-03-04 00:08:13 +0100

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,2 +1,1 @@
-This is a test application for reservation places to movie in Cinema.
-The purpose of the application was to learn python OOP. I hope that this application will be constantly uppdated.
+
